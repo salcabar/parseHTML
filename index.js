7033b3c8ac4c16c9fbf874a43b756793fdcf7a8c
salcabar <salcabar@126.com>
2016-05-13 17:56:05 +0800

    解析了所有节点所有属性

--- index.js
+++ index.js
@@ -1,54 +1,67 @@
-function DOMtoString(root, config) {
+const selfAttr = ['id', 'class', 'src', 'href', 'type', 'rel', 'style', 'alt', 'title', 'target'];
 
-    var json = mapDOM(root, true);
-    var arr = JSON.parse(json);
+console.log(DOMtoTree(document.body));
+
+function DOMtoTree (root, config) {
+
+    let data = mapDOM(root, true);
+    let arr = JSON.parse(data);
 
     return arr;
+}
 
-    function mapDOM(element, json) {
-        var treeObject = {};
+function mapDOM (element, isJSON) {
+    let treeObject = {};
+    let docNode = null;
+    let parser = null;
 
-        if (typeof element === "string") {
-            if (window.DOMParser) {
-                parser = new DOMParser();
-                docNode = parser.parseFromString(element, "text/html");
-                console.log(docNode)
-            }
-            element = docNode.firstChild;
+    if (typeof element === 'string') {
+        if (window.DOMParser) {
+            parser = new DOMParser();
+            docNode = parser.parseFromString(element, 'text/html');
+            console.log(docNode)
         }
-
-        treeHTML(element, treeObject);
-
-        return (json) ? JSON.stringify(treeObject) : treeObject;
+        element = docNode.firstChild;
     }
 
-    function treeHTML(element, object) {
-        object["nodeName"] = element.nodeName;
-        object["tagName"] = element.tagName;
-        var nodeList = element.childNodes;
-        if (nodeList != null) {
-            if (nodeList.length) {
-                object["children"] = [];
-                for (var i = 0; i < nodeList.length; i++) {
-                    if (nodeList[i].nodeType == 3) {//文本节点
-                        object["children"].push(nodeList[i].nodeValue);
-                    } else {
-                        object["children"].push({});
-                        treeHTML(nodeList[i], object["children"][object["children"].length - 1]);
-                    }
+    treeHTML(element, treeObject);
+
+    return (isJSON) ? JSON.stringify(treeObject) : treeObject;
+}
+
+function treeHTML (element, object) {
+    
+    element['depth'] = (element.parentNode.depth !== undefined) ? (element.parentNode.depth + 1) : 0;
+    object['depth'] = element.depth;
+    object['nodeName'] = element.nodeName;
+    object['tagName'] = element.tagName;
+    object['children'] = [];
+    object['attr'] = {};
+    
+    let children = object['children'];
+    const nodeList = element.childNodes;
+    const attr = element.attributes;
+    
+    if (nodeList != null && nodeList.length) {
+        for (var i = 0; i < nodeList.length; i++) {
+            if (nodeList[i].nodeType === 3) { // 文本节点
+                if(nodeList[i].nodeValue.trim() !== '') {
+                    children.push({'text': nodeList[i].nodeValue});
                 }
+            } else {
+                children.push({});
+                treeHTML(nodeList[i], children[children.length - 1]);
             }
         }
-        if (element.attributes != null) {
-            if (element.attributes.length) {
-                object["attr"] = {};
-                for (var i = 0; i < element.attributes.length; i++) {
-                    object["attr"][element.attributes[i].nodeName] = element.attributes[i].nodeValue;
-                }
-            }
+    }
+    
+    if (attr != null && attr.length) {
+        
+        for (let i = 0; i < attr.length; i++) {
+            for(let j = 0; j < selfAttr.length; j++) {
+                (attr[i].nodeName === selfAttr[j]) && (object[selfAttr[j]] = attr[i].nodeValue);
+            }      
+            (object[attr[i].nodeName] === undefined) && (object.attr[attr[i].nodeName] = attr[i].nodeValue);
         }
     }
 }
-
-
-console.log(DOMtoString(dom_string))
